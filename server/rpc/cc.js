e5db9234b72f1cc42002387206f304b40d3b7e99
Waxolunist <christian.sterzl@gmail.com>
2013-04-03 14:45:54 +0200

    isloaded

--- server/rpc/cc.js
+++ server/rpc/cc.js
@@ -5,73 +5,66 @@ if (typeof define !== 'function') {
 define(function(require) {
   var dejavu = require('dejavu');
 
+  function getPropertyByString(o, s) {
+    s = s.replace(/\[(\w+)\]/g, '.$1');  // convert indexes to properties
+    s = s.replace(/^\./, ''); // strip leading dot
+    var a = s.split('.');
+    while (a.length) {
+        var n = a.shift();
+        if (n in o) {
+            o = o[n];
+        } else {
+            return;
+        }
+    }
+    return o;
+  }
+
+  Object.prototype.byString = function(s) {
+    return getPropertyByString(this, s);
+  };
+
   var cc = {};
 
-  /* Interfaces */
-  cc.IResource = dejavu.Interface.declare({
-    $constants: {
-      ITEMTYPE: 'item',
-      LISTTYPE: 'list'
-    },
-
-    getUri: function () {},
-    getPath: function () {},
-    getType: function () {},
-    getSuperType: function () {},
-    getTemplates: function () {},
-  });
-
-  /* Abstract implementations */
-  cc.AbstractResource = dejavu.AbstractClass.declare({
-    $implements: [cc.IResource],
-
-    _uri: null,
-    _path: null,
-    _type: null,
-    _supertype: null,
-    _templates: null,
-
-    getUri: function () { return this._uri; },
-    setUri: function (uri) { this._uri = uri; return this; },
-
-    getPath: function () { return this._path; },
-    setPath: function (path) { this._path = path; return this; },
-
-    getType: function () { return this._type; },
-    setType: function (type) { this._type = type; return this; },
-
-    getSuperType: function () { return this._supertype; },
-    setSuperType: function (supertype) { this._supertype = supertype; return this; },
-
-    getTemplates: function () { return this._templates; },
-    setTemplates: function (templates) { this._templates = templates; return this; },
-  });
-
-  /* Implementations */
-  cc.Item = dejavu.Class.declare({
-    $extends: cc.AbstractResource,
-
-    initialize: function(uri, path, type) {
-      this.setSuperType(this.$static.ITEMTYPE);
-
-      this.setUri(uri);
-      this.setPath(path);
-      this.setType(type ? type : this.getSuperType());
+  cc.isLoaded = function(pkg, a) {
+    if(!a || !pkg) {
+      return false;
+    } else if(typeof a === 'string') {
+      return cc.isLoaded(pkg, a.split('.'));
+    } else if(!(a instanceof Array)) {
+      return false;
+    } else {
+      if(a.length == 1) {
+        return !!pkg[a[0]];
+      }
+      return cc.isLoaded(pkg[a[0]], a.slice(1, a.length));
     }
-  });
+  }
 
-  cc.List = dejavu.Class.declare({
-    $extends: cc.AbstractResource,
-
-    initialize: function(uri, path, type) {
-      this.setSuperType(this.$static.LISTTYPE);
-
-      this.setUri(uri);
-      this.setPath(path);
-      this.setType(type ? type : this.getSuperType());
+  return function() {
+    for(var i = 0; i < arguments.length; i++) {
+      var pkgname = arguments[i];
+      console.log('Load package ' + pkgname + ' ...');
+      
+      var pkgnamesplit = pkgname.split('.');
+      pkgnamesplit.forEach(function(el, idx, array) {
+        var parentpkgname = pkgnamesplit.slice(0, idx);
+        if(cc[el]) {
+          console.log('Package ' + el + ' already loaded. Do nothing!');
+        } else {
+          
+        }
+      });
+      var module = require('./cc.' + pkgname + '.js');
+      if(!module) {
+        console.log('Could not find package ' + pkgname);
+      } else {
+        console.log('Package ' + pkgname + ' successful loaded.');
+        cc[pkgname] = module;
+      }
     }
-  });
-
-  return cc;
+    
+    return cc;
+  };
 //define end
 });

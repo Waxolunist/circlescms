d30250ed207954039c567bbf884128e96040ccaa
Christian Sterzl <christian.sterzl@gmail.com>
2013-04-17 00:09:50 +0200

    Deleted node_modules, set contentdir

--- server/rpc/cms.js
+++ server/rpc/cms.js
@@ -6,7 +6,7 @@ exports.actions = function(req, res, ss) {
   req.use('session');
 
   var factory = new cc.resource.provider.ResourceFactory();
-  var fsprovider = new cc.resource.provider.fs.FsResourceProvider("fs", "../circlescms-content", function() {});
+  var fsprovider = new cc.resource.provider.fs.FsResourceProvider("fs", "content", function() {});
   cc.resource.provider.ProviderRegistry.getInstance().registerProvider(fsprovider.getName(), fsprovider);
   cc.parser.ParserRegistry.getInstance().registerParser('md', new cc.parser.markdown.MarkdownParser());
 

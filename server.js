ac6c5890d7cc3190e17401d724db71cc62ffee06
Christian Sterzl <christian.sterzl@gmail.com>
2013-05-30 08:49:30 +0200

    Testing behind proxy

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ ss.responders.add(require('ss-angular'), {pollFreq: 1000});
 if (process.env.OPENSHIFT_NODEJS_PORT) {
   ss.ws.transport.use('engineio', {
     client: {
-      port: 80,
+      port: 8000,
       transports: ['websocket', 'polling', 'flashsocket']
     }
   });
